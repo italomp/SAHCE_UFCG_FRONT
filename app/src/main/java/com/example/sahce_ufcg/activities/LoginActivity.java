49587cebf309b5cf327a3bd3ca362b5f7d49c414
Italo Modesto Pereira <italo.pereira@ccc.ufcg.edu.br>
2022-11-02 20:54:08 -0300

    login works with basic auth

--- app/src/main/java/com/example/sahce_ufcg/activities/LoginActivity.java
+++ app/src/main/java/com/example/sahce_ufcg/activities/LoginActivity.java
@@ -9,7 +9,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sahce_ufcg.R;
+import com.example.sahce_ufcg.models.User;
+import com.example.sahce_ufcg.services.ApiService;
 import com.google.android.material.textfield.TextInputEditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
     TextInputEditText emailEditText, passwordEditText;
@@ -26,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     public void setViews(){
         emailEditText = findViewById(R.id.email_field);
         passwordEditText = findViewById(R.id.password_field);
-        loginButton = findViewById(R.id.login_button);
+        setLoginButton();
         setSignupTextView();
     }
 
@@ -36,6 +42,36 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), UserRegisterActivity.class));
+            }
+        });
+    }
+
+    public void setLoginButton(){
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = String.valueOf(emailEditText.getText());
+                String password = String.valueOf(passwordEditText.getText());
+                System.out.println(email + password); // REMOVERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+                User newUser = new User(email, password);
+                ApiService.getUserService().login(newUser).enqueue(
+                        new Callback<User>() {
+                            @Override
+                            public void onResponse(Call<User> call, Response<User> response) {
+                                System.out.println(response.code());
+                                System.out.println(response.message());
+                                System.out.println(response.headers());
+
+
+                            }
+
+                            @Override
+                            public void onFailure(Call<User> call, Throwable t) {
+                                System.out.println("onFailure");
+                            }
+                        }
+                );
             }
         });
     }
--- app/src/main/java/com/example/sahce_ufcg/services/UserService.java
+++ app/src/main/java/com/example/sahce_ufcg/services/UserService.java
@@ -6,9 +6,16 @@ import com.example.sahce_ufcg.responseBodies.UserResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
-
 public interface UserService {
 
-    @POST("/users/")
+    @POST("/v1/users/")
     Call<UserResponseBody> createUser(@Body User user);
+
+//    @FormUrlEncoded
+//    @Headers("Content-Type: application/x-www-form-urlencoded")
+//    @POST("/v1/anonymous/login")
+//    Call<HttpHeaders> login(@Field("username") String username, @Field("password") String password);
+
+    @POST("/v1/anonymous/login")
+    Call<User> login(@Body User user);
 }
