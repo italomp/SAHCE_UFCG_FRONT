7ddef323b60a22e4a21bb64f58a6142a157f9bed
Italo Modesto Pereira <italo.pereira@ccc.ufcg.edu.br>
2022-11-03 21:29:29 -0300

    saving token in sharedPreferences

--- app/src/main/java/com/example/sahce_ufcg/activities/LoginActivity.java
+++ app/src/main/java/com/example/sahce_ufcg/activities/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.sahce_ufcg.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -59,7 +61,9 @@ public class LoginActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(Call<Void> call, Response<Void> response) {
                                 if(response.isSuccessful()){
-                                    System.out.println(response.headers().get("Authorization"));
+                                    String token = response.headers().get("Authorization");
+                                    saveToken(token);
+                                    goToDashboard();
                                 }
                                 else{
                                     System.out.println("Status Code: " + response.code());
@@ -75,5 +79,16 @@ public class LoginActivity extends AppCompatActivity {
                 );
             }
         });
+    }
+
+    public void saveToken(String token){
+        SharedPreferences sharedPreferences = this.getSharedPreferences("loginPreferences",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(String.valueOf(R.string.token), token);
+        editor.apply();
+    }
+
+    public void goToDashboard(){
+        startActivity(new Intent(getApplicationContext(), Dashboard.class));
     }
 }
