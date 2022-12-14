d7f5d99dc2758bb9c2173c344de7af300e5b1288
Italo Modesto Pereira <italo.pereira@virtus.ufcg.edu.br>
2022-12-14 09:21:12 -0300

    views of user details screen developed

--- app/src/main/java/com/example/sahce_ufcg/activities/UserDetailsActivity.java
+++ app/src/main/java/com/example/sahce_ufcg/activities/UserDetailsActivity.java
@@ -1,8 +1,12 @@
 package com.example.sahce_ufcg.activities;
 
+import static com.example.sahce_ufcg.models.User.UserType.INTERNAL_USER;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,22 +15,69 @@ import com.example.sahce_ufcg.models.User;
 import com.squareup.picasso.Picasso;
 
 public class UserDetailsActivity extends AppCompatActivity {
-    TextView textTestView;
-    ImageView imageTestView;
+    private TextView userNameView, userPhoneView, userAddressView, userEmailView, userTypeView;
+    private ImageView documentPictureView;
+    private Button validateButton;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
 
-        textTestView = findViewById(R.id.text_test);
-        imageTestView = findViewById(R.id.image_test);
+        user = (User) getIntent().getExtras().getSerializable("user");
+        setViews();
+    }
 
-        User user = (User) getIntent().getExtras().getSerializable("user");
-        textTestView.setText(user.getEmail());
+    public void setViews(){
+        setUserNameView();
+        setUserPhoneView();
+        setUerAddressView();
+        setUserEmailView();
+        setUserTypeView();
+        setDocumentPictureView();
+        setValidateButton();
+    }
 
+    public void setUserNameView(){
+        userNameView = findViewById(R.id.user_name_text);
+        userNameView.setText(user.getName());
+    }
+
+    public void setUserPhoneView(){
+        userPhoneView = findViewById(R.id.user_phone_text);
+        userPhoneView.setText(user.getPhone());
+    }
+
+    public void setUerAddressView(){
+        userAddressView = findViewById(R.id.user_address_text);
+        userAddressView.setText(user.getAddress());
+    }
+
+    public void setUserEmailView(){
+        userEmailView = findViewById(R.id.user_email_text);
+        userEmailView.setText(user.getEmail());
+    }
+
+    public void setUserTypeView(){
+        userTypeView = findViewById(R.id.user_type_text);
+        userTypeView.setText(user.getUserType() == INTERNAL_USER ? "Interna" : "Externa");
+    }
+
+    public void setDocumentPictureView(){
+        documentPictureView = findViewById(R.id.document_image);
         String requestParam = "?userEmail=" + user.getEmail();
         String uri = "http://10.0.2.2:8080/v1/anonymous/users/documentPicture" + requestParam;
-        Picasso.get().load(uri).into(imageTestView);
+        Picasso.get().load(uri).into(documentPictureView);
+    }
+
+    public void setValidateButton(){
+        validateButton = findViewById(R.id.btn_validate);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Enviar Request de Validação");
+            }
+        });
     }
 }
